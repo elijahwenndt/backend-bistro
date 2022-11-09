e7a6cfafbac4ab14d9a8c87ea38b97cfb5221a98
elijahwenndt <elijah.wenndt@student.awesomeinc.org>
2022-11-09 20:38:37 +0000

    admin site working now populating tables

--- myproject/backend_bistro/views.py
+++ myproject/backend_bistro/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, JsonResponse
+from .models import backend_bistro
 # Create your views here.
+
+def get_menu(request):
+    bistro_menu = list(backend_bistro.objects.values())
+
+    return JsonResponse ({'data': bistro_menu})
