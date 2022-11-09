3021cfb652cab674d998d85ad5406102c69aa2a1
elijahwenndt <elijah.wenndt@student.awesomeinc.org>
2022-11-09 19:51:13 +0000

    table strcutre set

--- myproject/backend_bistro/models.py
+++ myproject/backend_bistro/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+class Cuisine(models.Model):
+    types = models.CharField(max_length=50)
 # Create your models here.
+
+class MenuItem(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+    price = models.IntegerField(null=True, blank=True)
+    spice_level = models.IntegerField(null=True, blank=True)
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    cuisine_id = models.ForeignKey(Cuisine, on_delete=models.CASCADE)
