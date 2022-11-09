0e651fa59c97df5839e1ea25e8de8e6dc95ef21b
elijahwenndt <elijah.wenndt@student.awesomeinc.org>
2022-11-09 22:07:46 +0000

    generating table data on admin screen

--- myproject/backend_bistro/models.py
+++ myproject/backend_bistro/models.py
@@ -4,8 +4,14 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
 class Cuisine(models.Model):
     types = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.types
 # Create your models here.
 
 class MenuItem(models.Model):
@@ -15,3 +21,6 @@ class MenuItem(models.Model):
     spice_level = models.IntegerField(null=True, blank=True)
     category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
     cuisine_id = models.ForeignKey(Cuisine, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
